624fea7502317057c0e78f39a89a136f702d53c4
Kate Park <katepark@rice13.stanford.edu>
2018-03-17 10:50:34 -0700

    no matplot

--- emo_utils.py
+++ emo_utils.py
@@ -1,7 +1,6 @@
 import csv
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
 def read_glove_vecs(glove_file):
@@ -80,23 +79,7 @@ def print_predictions(X, pred):
     for i in range(X.shape[0]):
         print(X[i], label_to_emoji(int(pred[i])))
         
-        
-def plot_confusion_matrix(y_actu, y_pred, title='Confusion matrix', cmap=plt.cm.gray_r):
-    
-    df_confusion = pd.crosstab(y_actu, y_pred.reshape(y_pred.shape[0],), rownames=['Actual'], colnames=['Predicted'], margins=True)
-    
-    df_conf_norm = df_confusion / df_confusion.sum(axis=1)
-    
-    plt.matshow(df_confusion, cmap=cmap) # imshow
-    #plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(df_confusion.columns))
-    plt.xticks(tick_marks, df_confusion.columns, rotation=45)
-    plt.yticks(tick_marks, df_confusion.index)
-    #plt.tight_layout()
-    plt.ylabel(df_confusion.index.name)
-    plt.xlabel(df_confusion.columns.name)
-    
+           
     
 def predict(X, Y, W, b, word_to_vec_map):
     """
